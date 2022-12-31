39ea9a928b5841ea5ade77a99f6975096e8588af
mzncvmc <mzncvmc@gmail.com>
2022-12-31 22:31:05 +0900

    #8 컴포넌트 파일 구성하기

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expeneses/Expenses';
 
 function App() {
   const expenses = [
