4884e63811b222a5193f4c8b04c614060140edb8
mzncvmc <mzncvmc@gmail.com>
2022-12-31 21:19:52 +0900

    #3 add css

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
     return (
-        <h2>ExpenseItem</h2>
+        <div className='expense-item'>
+            <div>2022.10.29</div>
+            <div className='expense-item__description'>
+                <h2>santnfe</h2>
+                <div className='expense-item__price'>$200.21</div>
+            </div>
+        </div>
     );
 } 
 
