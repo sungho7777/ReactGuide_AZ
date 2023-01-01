a2f006678a31b30574f00668e8c1945ea5461c2d
mzncvmc <mzncvmc@gmail.com>
2023-01-01 22:14:05 +0900

    #20 조건 명령문 반환 추가하기

--- 01-starting-setup/src/components/Expeneses/Expenses.js
+++ 01-starting-setup/src/components/Expeneses/Expenses.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
@@ -14,18 +15,6 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter((expense) => {
         return expense.date.getFullYear().toString() === filterYeared;
     });
-
-    let expensesContent = <p>No expense found.</p>;
-    if(filteredExpenses.length > 0){
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem 
-                key={expense.id}
-                title={expense.title} 
-                amount={expense.amount} 
-                date={expense.date}
-            />
-        ));
-    }
     
     return (
         <div>
@@ -33,7 +22,7 @@ const Expenses = (props) => {
                 <ExpensesFilter 
                     selected={filterYeared}
                     onChangeFilter={filterChangeHandler} />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
