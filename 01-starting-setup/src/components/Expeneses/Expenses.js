3d9def287e35153c10517455759110c9bd642e7b
mzncvmc <mzncvmc@gmail.com>
2023-01-01 21:50:18 +0900

    #19 조건부 내용 출력하기

--- 01-starting-setup/src/components/Expeneses/Expenses.js
+++ 01-starting-setup/src/components/Expeneses/Expenses.js
@@ -8,8 +8,6 @@ const Expenses = (props) => {
     const [filterYeared, setFilterYeared] = useState('2023');
 
     const filterChangeHandler = selectedYear => {
-        console.log('Expenses.js');
-        console.log(selectedYear);
         setFilterYeared(selectedYear);
     };
 
@@ -17,21 +15,25 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filterYeared;
     });
 
+    let expensesContent = <p>No expense found.</p>;
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}
+            />
+        ));
+    }
+    
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter 
                     selected={filterYeared}
                     onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date}
-                    />
-                ))};
-
+                {expensesContent}
             </Card>
         </div>
     );
