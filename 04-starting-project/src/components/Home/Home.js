f6c51d629443cad8bfefd539e015985dbafc9d1c
mzncvmc <mzncvmc@gmail.com>
2023-01-19 21:55:42 +0900

    #9 사용자 정의 컨텍스트 제공자 구성요소 빌드 및 사용

--- 04-starting-project/src/components/Home/Home.js
+++ 04-starting-project/src/components/Home/Home.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const Home = (props) => {
-  return (
-    <Card className={classes.home}>
-      <h1>Welcome back!</h1>
-        <Button onClick={props.onLogout}>Logout</Button>
-    </Card>
-  );
+    const authCtx = useContext(AuthContext);
+
+    return (
+        <Card className={classes.home}>
+            <h1>Welcome back!</h1>
+            <Button onClick={authCtx.onLogout}>Logout</Button>
+        </Card>
+    );
 };
 
 export default Home;
