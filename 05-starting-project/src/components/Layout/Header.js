663f4371b1fbd267e2a97030e21bce3dbc4f459d
mzncvmc <mzncvmc@gmail.com>
2023-01-21 13:07:08 +0900

    #3 장바구니 컴포넌트 추가하기

--- 05-starting-project/src/components/Layout/Header.js
+++ 05-starting-project/src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import {Fragment} from 'react';
 
 import mealsImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 
 const Header = (props) => {
@@ -9,7 +10,7 @@ const Header = (props) => {
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImg} alt='A table full of delicious food!' />
