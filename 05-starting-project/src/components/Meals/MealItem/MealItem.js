0addc10ed4b90e9e3b031cae469b1611cbb9f724
mzncvmc <mzncvmc@gmail.com>
2023-01-24 11:45:03 +0900

    #6 양식 추가하기

--- 05-starting-project/src/components/Meals/MealItem/MealItem.js
+++ 05-starting-project/src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,6 @@
+import MealItemForm from "./MealItemForm";
 import classes from './MealItem.module.css';
+
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -11,7 +13,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm />
             </div>
         </li>
     );
