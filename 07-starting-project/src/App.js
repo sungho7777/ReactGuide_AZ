0ba4561426d251c0cad3ebb8dd388d4e5f70b4e6
mzncvmc <mzncvmc@gmail.com>
2023-01-28 17:50:45 +0900

    #7 firebasedatabase() POST * GET 요청하기

--- 07-starting-project/src/App.js
+++ 07-starting-project/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from "./components/AddMovie";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -13,22 +14,24 @@ function App() {
         setError(null);
 
         try {
-            const response = await fetch('https://swapi.dev/api/films');
+            const response = await fetch('https://reactguide-az-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
             if (!response.ok) {
                 throw new Error('Something went wrong!');
             }
 
             const data = await response.json();
-            const transformedMovies = data.results.map(movieData => {
-                return {
-                    id         : movieData.episode_id,
-                    title      : movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date
-                };
-            });
+            const loadedMovies = [];
 
-            setMovies(transformedMovies);
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
+
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -38,6 +41,15 @@ function App() {
     useEffect(() => {
         fetchMovieHandler();
     }, [fetchMovieHandler]);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://reactguide-az-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json', {
+            method: 'POST',
+            body: JSON.stringify(movie)
+        });
+        const data = await response.json();
+        console.log(data);
+    }
 
     let content = <p>Found no movies.</p>;
     if(movies.length > 0){
@@ -53,6 +65,9 @@ function App() {
 
     return (
         <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
             <section>
                 <button onClick={fetchMovieHandler}>Fetch Movies</button>
             </section>
