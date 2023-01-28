bbb9487282b16bb2bea181a807871cf73a93cdce
mzncvmc <mzncvmc@gmail.com>
2023-01-28 17:23:55 +0900

    #6 useEffect(), useCallback() 요청  사용하기

--- 07-starting-project/src/App.js
+++ 07-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,13 +8,13 @@ function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    async function fetchMovieHandler() {
+    const fetchMovieHandler = useCallback(async () => {
         setIsLoading(true);
         setError(null);
 
-        try{
+        try {
             const response = await fetch('https://swapi.dev/api/films');
-            if(!response.ok){
+            if (!response.ok) {
                 throw new Error('Something went wrong!');
             }
 
@@ -29,11 +29,15 @@ function App() {
             });
 
             setMovies(transformedMovies);
-        }catch (error){
+        } catch (error) {
             setError(error.message);
         }
         setIsLoading(false);
-    }
+    }, []);
+
+    useEffect(() => {
+        fetchMovieHandler();
+    }, [fetchMovieHandler]);
 
     let content = <p>Found no movies.</p>;
     if(movies.length > 0){
